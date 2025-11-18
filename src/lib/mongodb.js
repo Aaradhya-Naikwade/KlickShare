a9bf4eff4619b4980797abed03e2c48f5ac31a56
Aaradhya Naikwade <aaradhyanaikwade08@gmail.com>
2025-11-18 20:51:03 +0530

    vercel deployment

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb+srv://aaradhyanaikwade2520_db_user:ehxPQkSvn5RlsNEq@cluster0.ewtpwpx.mongodb.net/?appName=Cluster0";
+// const MONGODB_URI = "mongodb+srv://aaradhyanaikwade2520_db_user:ehxPQkSvn5RlsNEq@cluster0.ewtpwpx.mongodb.net/?appName=Cluster0";
+
+// Ab yeh value .env.local (local) ya Vercel settings (live) se uthayega
+const MONGODB_URI = process.env.MONGODB_URI; 
+
+// Optional: Agar MONGODB_URI set nahi hai toh error throw karein
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI environment variable is not set.");
+}
 
 // Prevent multiple connections during development (Next.js hot reload fix)
 let isConnected = false;
